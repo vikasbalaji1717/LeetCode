2d5ba6fca583a3f4c080756c18b9f43d49179d82
vikasbalaji1717 <vikasbalaji1717@gmail.com>
2026-09-22 23:52:14 +0530

    Time: 871 ms (25.73%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,15 @@
+SELECT
+    s.product_id,
+    s.year AS first_year,
+    s.quantity,
+    s.price
+FROM Sales s
+JOIN (
+    SELECT
+        product_id,
+        MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+) f
+ON s.product_id = f.product_id
+AND s.year = f.first_year;
